f49dd3d2238f263671ef97658e35683054e58267
thaliaberger <thaliaberger@hotmail.com>
2021-01-10 12:54:42 -0300

    links ok

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const doc = document.body;
+const arrow = document.getElementById("arrow");
 const chevron = document.getElementById("chevron");
 
 window.onscroll = () => handleScroll();
 
 function handleScroll() {
+  doc.scrollTop < 100
+    ? (chevron.style.opacity = "0")
+    : (chevron.style.opacity = "1");
+
   doc.scrollTop > 1000 && doc.scrollTop < 1500
-    ? (chevron.style.display = "block")
-    : (chevron.style.display = "none");
+    ? (arrow.style.display = "block")
+    : (arrow.style.display = "none");
 }
 
 window.onload = () => {
   document.getElementById("sobre").onclick = () => {
-    window.scrollTo({ top: 560, behavior: "smooth" });
+    window.scrollTo({ top: 600, behavior: "smooth" });
   };
 
   document.getElementById("projetos").onclick = () => {
-    window.scrollTo({ top: 1200, behavior: "smooth" });
+    window.scrollTo({ top: 1250, behavior: "smooth" });
+  };
+
+  document.getElementById("chevron").onclick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 };
