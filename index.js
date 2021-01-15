2ef141ea7e7850ee65ae36a3fe44253ebc7180b7
thaliaberger <thaliaberger@hotmail.com>
2021-01-15 11:56:44 -0300

    done

--- index.js
+++ index.js
@@ -15,13 +15,29 @@ function handleScroll() {
 }
 
 window.onload = () => {
-  document.getElementById("sobre").onclick = () => {
-    window.scrollTo({ top: 600, behavior: "smooth" });
-  };
+  if (window.matchMedia("(max-width: 340px)").matches) {
+    document.getElementById("sobre").onclick = () => {
+      window.scrollTo({ top: 100, behavior: "smooth" });
+    };
+  } else if (window.matchMedia("(max-width: 640px)").matches) {
+    document.getElementById("sobre").onclick = () => {
+      window.scrollTo({ top: 250, behavior: "smooth" });
+    };
+  } else {
+    document.getElementById("sobre").onclick = () => {
+      window.scrollTo({ top: 600, behavior: "smooth" });
+    };
+  }
 
-  document.getElementById("projetos").onclick = () => {
-    window.scrollTo({ top: 1250, behavior: "smooth" });
-  };
+  if (window.matchMedia("(max-width: 640px)").matches) {
+    document.getElementById("projetos").onclick = () => {
+      window.scrollTo({ top: 820, behavior: "smooth" });
+    };
+  } else {
+    document.getElementById("projetos").onclick = () => {
+      window.scrollTo({ top: 1250, behavior: "smooth" });
+    };
+  }
 
   document.getElementById("chevron").onclick = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
